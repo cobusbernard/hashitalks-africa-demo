609eab119403a06df2564d5d00f66e3c08ad11ba
Cobus Bernard <cobusb@amazon.com>
2020-07-15 13:25:11 +0000

    Adding modified files.

--- main/providers.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  version                 = "~> 2.0"
-  region                  = var.aws_region
-  profile                 = "default"
-  shared_credentials_file = "/home/ubuntu/.aws/credentials_hashitalks"
-}
