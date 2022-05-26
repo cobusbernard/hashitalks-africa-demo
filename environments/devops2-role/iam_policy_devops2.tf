3335fd84a3e1fa4e23a3174016e9fc42d05d5ee6
Amit Karpe <amitkarpe@gmail.com>
2022-05-26 19:04:45 +0800

    Moved all "devops2" related config into devops2-role

---
+++ environments/devops2-role/iam_policy_devops2.tf
@@ -0,0 +1,19 @@
+# This temporary deveops2 role's policy
+resource "aws_iam_policy" "full" {
+  name        = "full_access"
+  path        = "/"
+  description = "Full access policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "*",
+      "Resource": "*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
